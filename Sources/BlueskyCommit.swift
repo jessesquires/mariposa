b285035e9b705af13abf2474e1d38bbb2c453ac1
Jesse Squires <jesse@jessesquires.com>
2025-05-26 14:36:46 -0700

    parse record response

---
+++ Sources/BlueskyCommit.swift
@@ -0,0 +1,16 @@
+//
+//  Created by Jesse Squires.
+//  https://www.jessesquires.com
+//
+//  GitHub
+//  https://github.com/jessesquires/mariposa
+//
+//  Copyright © 2025-present Jesse Squires
+//
+
+import Foundation
+
+struct BlueskyCommit: Hashable, Codable {
+    let cid: String
+    let rev: String
+}
---
+++ Sources/BlueskyRecordCreated.swift
@@ -0,0 +1,18 @@
+//
+//  Created by Jesse Squires.
+//  https://www.jessesquires.com
+//
+//  GitHub
+//  https://github.com/jessesquires/mariposa
+//
+//  Copyright © 2025-present Jesse Squires
+//
+
+import Foundation
+
+struct BlueskyRecordCreated: Hashable, Codable {
+    let cid: String
+    let commit: BlueskyCommit
+    let uri: String
+    let validationStatus: String
+}
