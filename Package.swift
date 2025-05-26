370cd0db3d3e477968805631fc24a44e42df53f5
Jesse Squires <jesse@jessesquires.com>
2025-05-26 12:29:10 -0700

    add initial empty package

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+//  GitHub
+//  https://github.com/jessesquires/mariposa
+//
+//  Copyright © 2024 Jesse Squires
+//  https://www.jessesquires.com
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "Mariposa",
+    platforms: [
+        .macOS(.v14)
+    ],
+    products: [
+        .executable(name: "mariposa", targets: ["Mariposa"])
+    ],
+    targets: [
+        .executableTarget(
+            name: "Mariposa",
+            path: "Sources"
+        )
+    ],
+    swiftLanguageModes: [.v6]
+)
---
+++ Sources/main.swift
@@ -0,0 +1,12 @@
+#!/usr/bin/swift
+//
+//  GitHub
+//  https://github.com/jessesquires/mariposa
+//
+//  Copyright © 2024 Jesse Squires
+//  https://www.jessesquires.com
+//
+
+import Foundation
+
+print("hello mariposa")
